9e3d4c4bfcd4c0ab9217ce58a983e6b1b15bd186
lydia-fastpage <lydia_cho@icloud.com>
2022-11-01 14:04:57 -0700

    faq

---
+++ faq.py
@@ -0,0 +1,13 @@
+import random
+
+faq_data = []
+faq_list = [
+    "Q: How do you play Snake? A: Move the snake around to eat the blocks!",
+    "Q: How do you move the snake? A: Use the arrow keys to move.",
+    "Q: Can you move through the snakes body? A: If you hit the snake's tail, you lose!",
+    "Q: What happens if you run into the border? A: You lose if your snake hits the border :(",
+    "Q: How do you win Snake? Win snake by getting all the blocks!",
+    "Q: Is snake fun? A: Yes! Snake is a super fun interactive game",
+    "Q: What College Board criteria does this game meet? A: All because we just amazing like that"
+    "Q: Who is the coolest teacher? A: Mr. Yeung of course"
+]
