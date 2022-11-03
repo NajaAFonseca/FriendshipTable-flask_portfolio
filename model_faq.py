3fb3dceaaadc0ee4c7f45a51e936a11730c9e32b
lydia-fastpage <lydia_cho@icloud.com>
2022-11-02 19:20:23 -0700

    faq

--- model_faq.py
+++ model_faq.py
@@ -8,8 +8,8 @@ faq_list = [
     "Q: What happens if you run into the border? A: You lose if your snake hits the border :(",
     "Q: How do you win Snake? Win snake by getting all the blocks!",
     "Q: Is snake fun? A: Yes! Snake is a super fun interactive game",
-    "Q: What College Board criteria does this game meet? A: All because we just amazing like that"
-    "Q: Who is the coolest teacher? A: Mr. Yeung of course'
+    "Q: What College Board criteria does this game meet? A: All because we just amazing like that",
+    "Q: Who is the coolest teacher? A: Mr. Yeung of course"
 ]
 
 # Initialize jokes
